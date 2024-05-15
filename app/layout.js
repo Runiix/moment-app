d1e39742878ff3795164df4775ee305b12756654
Runiix <rubenliebert@gmail.com>
2024-05-15 11:59:31 +0200

    meta description

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
       <html lang="en">
          <head>
             <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1.0"
+               name="description"
+               content="Moment-app Movie Entertainment application, 2024, Germany, created by ruben liebert"
             />
          </head>
          <body className={inter.className}>{children}</body>
